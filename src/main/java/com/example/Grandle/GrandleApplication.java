695f1a6e0f30e8ab6ad3291bce2c654ebea11cb5
SAMSOKREAKSMEY <127179736+SAMSOKREAKSMEY@users.noreply.github.com>
2024-03-29 17:26:09 +0700

    grandle

---
+++ src/main/java/com/example/Grandle/GrandleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Grandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Grandle/GrandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
